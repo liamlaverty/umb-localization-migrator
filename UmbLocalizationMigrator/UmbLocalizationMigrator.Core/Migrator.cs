b8b462ec21d85db36309a8a345ac0cf192261d0d
LFLaverty <liamlaverty@gmail.com>
2024-04-17 16:03:18 +0100

    handles the weird edge cases, where some of the generated properties start with `2`

--- UmbLocalizationMigrator/UmbLocalizationMigrator.Core/Migrator.cs
+++ UmbLocalizationMigrator/UmbLocalizationMigrator.Core/Migrator.cs
@@ -16,7 +16,7 @@ namespace UmbLocalizationMigrator.Core
     {
         public void MigrateDirectoryFromXmlToJson(string xmlFolderPath, string jsonFolderPath)
         {
-            IEnumerable<string> xmlFiles = Directory.GetFiles(xmlFolderPath, "*.xml");
+            IEnumerable<string> xmlFiles = Directory.GetFiles(xmlFolderPath, "en_us.xml");
 
             foreach (var file in xmlFiles)
             {
@@ -53,8 +53,14 @@ namespace UmbLocalizationMigrator.Core
 
                 foreach (var key in area.key)
                 {
-                    // Console.WriteLine($"Found key {key.alias}:{key.Value}");
-                    File.AppendAllText(filePath, $"{key.alias}: {JsonConvert.SerializeObject(key.Value)},\r\n");
+                    if (!IsSpecialKey(key.alias))
+                    {
+                        File.AppendAllText(filePath, $"{key.alias}: {JsonConvert.SerializeObject(key.Value)},\r\n");
+                    }
+                    else
+                    {
+                        File.AppendAllText(filePath, GetSpecialKeyText(key.alias, key.Value));
+                    }
                 }
 
                 File.AppendAllText(filePath, "\r\n},\r\n");
@@ -68,7 +74,31 @@ namespace UmbLocalizationMigrator.Core
 
         }
 
+        private string GetSpecialKeyText(string key, string value)
+        {
+            return $"{JsonConvert.SerializeObject(key)}: {JsonConvert.SerializeObject(value)},\r\n";
+        }
 
-
+        private bool IsSpecialKey(string alias)
+        {
+            switch (alias)
+            {
+                case "2fa":
+                case "2faDisableText":
+                case "2faProviderIsEnabled":
+                case "2faProviderIsDisabledMsg":
+                case "2faProviderIsNotDisabledMsg":
+                case "2faDisableForUser":
+                case "2faTitle":
+                case "2faText":
+                case "2faMultipleText":
+                case "2faCodeInput":
+                case "2faCodeInputHelp":
+                case "2faInvalidCode":
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
